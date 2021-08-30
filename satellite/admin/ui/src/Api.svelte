06d39326d97a6d9fda0840fb4c5b38cf222b92e0
Ivan Fraixedes <ivan@fraixed.es>
2021-08-30 15:21:41 +0200

    satellite/admin/ui: Create UI Generator component
    This commit creates some Svelte components in companion of some
    Typescript code which allows to generate a simple HTML page based on a
    opinionated definition of API.
    
    The commit contains an API endpoint definition to show how the UI
    generator components works.
    
    Change-Id: I296e0d5b73f4d026fc79b391cf9f01aa2caeca50

---
+++ satellite/admin/ui/src/Api.svelte
@@ -0,0 +1,76 @@
+<script lang="ts">
+  import type { Operation } from "./ui-generator";
+  import { Admin } from "./api";
+
+  import UIGen from "./UIGenerator.svelte";
+
+  const baseURL = `${window.location.protocol}//${window.location.host}/api`;
+  let api: Admin;
+  let selectedGroupOp: Operation[];
+  let selectedOp: Operation;
+  let authToken: string;
+
+  function confirmAuthToken() {
+    if (authToken) {
+      api = new Admin(baseURL, authToken);
+    } else {
+      api = null;
+    }
+  }
+</script>
+
+<p>
+  In order to use the API you have to set the authentication token in the input
+  box and press enter or click the "confirm" button.
+</p>
+<p>
+  Token: <input
+    bind:value={authToken}
+    on:focus={() => {
+      api = null;
+
+      // This allows to select the empty item of the second select.
+      selectedOp = null;
+    }}
+    on:keyup={(e) => {
+      if (e.keyCode === 13) confirmAuthToken();
+    }}
+    type="password"
+    size="48"
+  />
+  <button on:click={confirmAuthToken}>confirm</button>
+</p>
+
+{#if api}
+  <p>
+    Operation:
+    <select
+      bind:value={selectedGroupOp}
+      on:change={() => {
+        // This allows hiding the UIGen component when this select change until
+        // a new operations is selected in the following select element and also
+        // selecting the empty item of the select.
+        selectedOp = null;
+      }}
+    >
+      <option selected />
+      {#each Object.keys(api.operations) as group}
+        <option value={api.operations[group]}>{group}</option>
+      {/each}
+    </select>
+    {#if selectedGroupOp}
+      <select bind:value={selectedOp}>
+        <option selected />
+        {#each selectedGroupOp as op (op)}
+          <option value={op}>{op.name}</option>
+        {/each}
+      </select>
+    {/if}
+  </p>
+  <hr />
+  <p>
+    {#if selectedOp}
+      <UIGen operation={selectedOp} />
+    {/if}
+  </p>
+{/if}
